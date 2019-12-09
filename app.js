96e2ec4a28a5095fbed3b2e9bf65a293feda8817
Mattia <gandola.mattia@gmail.com>
2019-12-09 19:15:14 +0000

    working on javascript

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
+const theForm = document.forms["macroForm"];
 const PC_KCALG = 4;
 const F_KCALG = 9;
-const pgDaily_p = document.getElementsByClassName('pgdaily');
-const pKcalDaily_p = document.getElementsByClassName('pkdaily');
-const ppercDaily_p = document.getElementsByClassName('ppdaily');
-const fgDaily_p = document.getElementsByClassName('fgdaily');
-const fKcalDaily_p = document.getElementsByClassName('fkdaily');
-const fpercDaily_p = document.getElementsByClassName('fpdaily')
-const cgDaily_p = document.getElementsByClassName('cgdaily');
-const cKcalDaily_p = document.getElementsByClassName('ckdaily');
-const cpercDaily_p = document.getElementsByClassName('cpdaily');
+
+var bodyWeightUnit = ['kg', 'lb'];
+var proteinPerKiloUnit = ['pgkg', 'pgLb'];
+var fatPerKiloUnit = ['fgKg', 'fgLb'];
+
+var pgDaily_p = document.getElementById('pgdaily');
+var pKcalDaily_p = document.getElementById('pkdaily');
+var ppercDaily_p = document.getElementById('ppdaily');
+
+var fgDaily_p = document.getElementById('fgdaily');
+var fKcalDaily_p = document.getElementById('fkdaily');
+var fpercDaily_p = document.getElementById('fpdaily')
+
+var cgDaily_p = document.getElementById('cgdaily');
+var cKcalDaily_p = document.getElementById('ckdaily');
+var cpercDaily_p = document.getElementById('cpdaily');
+ 
 var kcalDaily = document.getElementById("kcalDaily").value;
-var bodyWeight = document.getElementById("bodyWeight").value;
-var protein_gKg = document.getElementById("proteingKg").value;
+var bodyWeight = document.getElementById("bodyWeight");
+var protein_gKg = document.getElementById("proteingKg");
 var fat_gKg = document.getElementById("fatgKg").value;
+
+//example function
+
+/*function test() {
+    document.getElementById('pgdaily').innerHTML = product();
+}*/
+function test() {
+    pKcalDaily_p.innerHTML = calcDailyProKcal();
+}
+
+console.log(dailyProG);
+
+function product() {
+    x = bodyWeight.value;
+    y = protein_gKg.value;
+    return x * y;
+}
 
 // Calculate daily proteins kcal intake and create variable for daily P grams
 function calcDailyProKcal() {
-    var dailyProG = bodyWeight * protein_gKg;
+    var dailyProG = bodyWeight.value * protein_gKg.value;
     var dailyProKcal = dailyProG * PC_KCALG;
     return dailyProKcal;
 }
